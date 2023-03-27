66df074a6aab980f7603a383d6d60d2ceead4851
lichengcheng <2523826531@qq.com>
2023-03-27 09:58:01 +0800

    first commit

---
+++ src/components/commonList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="common-list">
+    <div class="common-item" v-for="item in commonList" :key="item.id">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    commonList: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
